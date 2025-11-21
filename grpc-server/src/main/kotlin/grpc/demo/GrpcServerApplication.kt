14429332e789dcf7220d0a89f4bf24bb6d2cc27e
ImmortalIdiot <max.krasulya@yandex.ru>
2025-11-21 12:34:26 +0300

    feat: add grpc server

---
+++ grpc-server/src/main/kotlin/grpc/demo/GrpcServerApplication.kt
@@ -0,0 +1,11 @@
+package grpc.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrpcServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<GrpcServerApplication>(*args)
+}
---
+++ grpc-server/src/main/kotlin/grpc/demo/UserServiceImpl.kt
@@ -0,0 +1,52 @@
+package grpc.demo
+
+import io.grpc.stub.StreamObserver
+import org.springframework.grpc.server.service.GrpcService
+import java.util.UUID
+import java.util.concurrent.ConcurrentHashMap
+
+@GrpcService
+class UserServiceImpl : UserServiceGrpc.UserServiceImplBase() {
+    private val users = ConcurrentHashMap<String, User>()
+
+    override fun createUser(request: CreateUserRequest, responseObserver: StreamObserver<CreateUserResponse>) {
+        val newUser = User.newBuilder()
+            .setUserId(UUID.randomUUID().toString())
+            .setName(request.name)
+            .setEmail(request.email)
+            .build()
+
+        users[newUser.userId] = newUser
+
+        println("Создан пользователь: ${newUser.name}")
+
+        val response = CreateUserResponse.newBuilder().setUser(newUser).build()
+        responseObserver.onNext(response)
+        responseObserver.onCompleted()
+    }
+
+    override fun getUserBadges(
+        request: GetUserBadgesRequest,
+        responseObserver: StreamObserver<GetUserBadgesResponse>
+    ) {
+        val userId = request.userId
+
+        println("Запрошены достижения для пользователя: $userId")
+        val badge1 = Badge.newBuilder()
+            .setBadgeId("b1")
+            .setName("Первый шаг")
+            .setDescription("Успешно создан аккаунт")
+            .build()
+
+        val badge2 = Badge.newBuilder()
+            .setBadgeId("b2")
+            .setName("Любопытный")
+            .setDescription("Запросил свои достижения")
+            .build()
+
+        val response = GetUserBadgesResponse.newBuilder().addAllBadges(listOf(badge1, badge2)).build()
+
+        responseObserver.onNext(response)
+        responseObserver.onCompleted()
+    }
+}
